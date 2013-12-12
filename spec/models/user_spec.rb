470d879e03648a43372d91a040cd640fc607f1d7
CCCos <cosmo1@live.cn>
2013-12-12 20:29:09 +0100

    finish chapter9

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,23 +14,34 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it {should respond_to(:remember_token)}
   it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
 
-    it { should be_valid }
+  it { should be_valid }
+  it { should_not be_admin}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin}
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-    describe "when name is too long" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
-    end
+  end
 
-      describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
+       foo@bar_baz.com foo@bar+baz.com]
+       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
@@ -46,7 +57,7 @@ describe User do
       end
     end
   end
-    describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.save
@@ -55,7 +66,7 @@ describe User do
     it { should_not be_valid }
   end
 
-    describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -64,7 +75,7 @@ describe User do
 
     it { should_not be_valid }
   end
-    describe "email address with mixed case" do
+  describe "email address with mixed case" do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
     it "should be saved as all lower-case" do
@@ -74,37 +85,37 @@ describe User do
     end
   end
   describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: " ", password_confirmation: " ")
-  end
-  it { should_not be_valid }
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
   end
   describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
   describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email) }
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
 
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
-  end
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
     end
   end
-     describe "with a password that's too short" do
-      before { @user.password = @user.password_confirmation = "a" * 5 }
-       it { should be_invalid }
-      end
-      describe "remember_token" do
-        before { @user.save}
-        its(:remember_token) {should_not be_blank}
-      end
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+  describe "remember_token" do
+    before { @user.save}
+    its(:remember_token) {should_not be_blank}
+  end
 end
